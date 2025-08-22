d3425a2fde4411a8f2dee48423fdcaac72b0a6d0
vachirasawin <vachirasawin.mah@gmail.com>
2025-08-22 19:10:17 +0700

    version 22.08.25.1

--- src/app/aboutus/page.jsx
+++ src/app/aboutus/page.jsx
@@ -4,6 +4,7 @@
 import React from "react";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
+import Link from "next/link";
 
 // import from components
 import Navbar from "../components/Navbar";
