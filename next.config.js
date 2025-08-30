f0f77267ead88995c07007e0021a1d027a8a7ecd
vachirasawin <vachirasawin.mah@gmail.com>
2025-08-30 21:47:59 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    images: {
+        domains: [
+            "lh3.googleusercontent.com",    // Google profile images
+            "avatars.githubusercontent.com", // GitHub profile images
+            "graph.facebook.com",           // Facebook profile images
+        ],
+    },
 };
 
 module.exports = nextConfig;
