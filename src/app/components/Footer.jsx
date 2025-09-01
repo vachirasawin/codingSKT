0effa4af93466d8c91e424b2f8636c484c6b55c9
vachirasawin <vachirasawin.mah@gmail.com>
2025-09-01 08:49:42 +0700

    version 1.9.25.1

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,13 +1,17 @@
 // import from Next.js
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import "aos/dist/aos.css";
 
 function Footer({ home, signIn, signUp, addInfo, profile, dashboard, session, aboutUs, privacyPolicy, userGuide, faq, news, ourTeam }) {
+    const [quickLink, setQuickLink] = useState(false);
+    const [information, setInformation] = useState(false);
+    const [contactInfo, setContactInfo] = useState(false);
+
     return (
         <div className = "px-4 border-t border-[#607D94] bg-[#002B4E]">
-            <div className = "container mx-auto justify-self-center flex flex-wrap gap-4 justify-around py-8 max-md:py-4 text-white">
+            <div className = "container mx-auto justify-self-center flex flex-wrap gap-4 justify-around py-8 max-md:py-4 text-white max-md:flex-col">
                 <div className = "flex flex-col gap-2">
                     <div className = "flex items-center gap-2">
                         <Image src = "/logo.png" unoptimized alt = "codingSKT Logo" width = {1000} height = {1000} className = "w-11"/>
@@ -20,10 +24,15 @@ function Footer({ home, signIn, signUp, addInfo, profile, dashboard, session, ab
                     </div>
                     <p className = "text-xs font-medium text-[#9497a1]">version 1.9.25.1</p>
                 </div>
-                <div className = "flex gap-8">
-                    <div className = "flex flex-col gap-4">
-                        <h1 className = "font-semibold text-sm">Quick link</h1>
-                        <div className = "flex flex-col gap-2 text-xs font-medium text-[#9497a1]">
+                <div className = "flex gap-8 max-md:flex-col max-md:gap-4">
+                    <div className = "flex flex-col gap-4 max-md:border-b border-[#f7f7f7] pb-4">
+                        <div className = "flex justify-between items-center">
+                            <h1 className = "font-semibold text-sm">Quick Link</h1>
+                            <div className = {`md:hidden h-4 w-4 flex justify-center items-center transition-all duration-500 ${quickLink && "rotate-180"}`} onClick = {() => setQuickLink(!quickLink)}>
+                                <i className = "fa-solid fa-angle-down"></i>
+                            </div>
+                        </div>
+                        <div className = {`flex-col gap-2 text-xs font-medium text-[#9497a1] max-md:pl-4 ${quickLink ? "max-md:flex" : "max-md:hidden"}`}>
                             <Link href = "/" className = {`${home && "text-white"}`}>Home</Link>
                             <Link href = "/about us" className = {`${aboutUs && "text-white"}`}>About Us</Link>
                             {session ? (
@@ -41,18 +50,28 @@ function Footer({ home, signIn, signUp, addInfo, profile, dashboard, session, ab
                             <Link href = "/privacy policy" className = {`${privacyPolicy && "text-white"}`}>Privacy Policy</Link>
                         </div>
                     </div>
-                    <div className = "flex flex-col gap-4">
-                        <h1 className = "font-semibold text-sm">Information</h1>
-                        <div className = "flex flex-col gap-2 text-xs font-medium text-[#9497a1]">
+                    <div className = "flex flex-col gap-4 max-md:border-b border-[#f7f7f7] pb-4">
+                        <div className = "flex justify-between items-center">
+                            <h1 className = "font-semibold text-sm">Information</h1>
+                            <div className = {`md:hidden h-4 w-4 flex justify-center items-center transition-all duration-500 ${information && "rotate-180"}`} onClick = {() => setInformation(!information)}>
+                                <i className = "fa-solid fa-angle-down"></i>
+                            </div>
+                        </div>
+                        <div className = {`flex-col gap-2 text-xs font-medium text-[#9497a1] max-md:pl-4 ${information ? "max-md:flex" : "max-md:hidden"}`}>
                             <Link href = "/user guide" className = {`${userGuide && "text-white"}`}>User Guide</Link>
                             <Link href = "/faqs" className = {`${faq && "text-white"}`}>FAQ</Link>
                             <Link href = "/news" className = {`${news && "text-white"}`}>News</Link>
                             <Link href = "/our team" className = {`${ourTeam && "text-white"}`}>Our Team</Link>
                         </div>
                     </div>
-                    <div className = "flex flex-col gap-4">
-                        <h1 className = "font-semibold text-sm">Contact info</h1>
-                        <div className = "flex flex-col gap-2 text-xs font-medium text-[#9497a1]">
+                    <div className = "flex flex-col gap-4 max-md:border-b border-[#f7f7f7] pb-4">
+                        <div className = "flex justify-between items-center">
+                            <h1 className = "font-semibold text-sm">Contact Info</h1>
+                            <div className = {`md:hidden h-4 w-4 flex justify-center items-center transition-all duration-500 ${contactInfo && "rotate-180"}`} onClick = {() => setContactInfo(!contactInfo)}>
+                                <i className = "fa-solid fa-angle-down"></i>
+                            </div>
+                        </div>
+                        <div className = {`flex-col gap-2 text-xs font-medium text-[#9497a1] max-md:pl-4 ${contactInfo ? "max-md:flex" : "max-md:hidden"}`}>
                             <p>tiyasak.srm@sk-thonburi.ac.th</p>
                             <p>ratchanon.tri@sk-thonburi.ac.th</p>
                             <p>vachirasawin.mah@sk-thonburi.ac.th</p>
